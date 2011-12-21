6af9d50237209a5b68e2d9aaba857d634827774f
Richard Race <rcr8@aber.ac.uk>
2011-12-21 17:07:37 +0000

    Removed native tab bar.

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,8 @@ class AppApplication < Rho::RhoApplication
   def initialize
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
     # Super must be called *after* settings @tabs!
-    @tabs = [
+    @tabs = nil
+=begin    [
       {:label => "Controller", :action => '/app/Controls'},
       {:label => "Movies", :action => '/app', :icon => '/public/images/icons/clapboard.png'},
       {:label => "TV Shows", :action => '/app', :icon => '/public/images/icons/tv-1.png'},
@@ -15,6 +16,7 @@ class AppApplication < Rho::RhoApplication
       {:label => "Music", :action => '/app', :icon => '/public/images/icons/note-2.png'},
       {:label => "Files", :action => '/app', :icon => '/public/images/icons/cabinet.png'}
     ]
+=end
     #To remove default toolbar uncomment next line:
     @@toolbar = nil
     super
