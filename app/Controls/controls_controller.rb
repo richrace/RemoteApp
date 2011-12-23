0a9b354bfe1bde327f7e7c187bd1e25ea54ae43a
Richard Race <rcr8@aber.ac.uk>
2011-12-23 13:56:46 +0000

    Refactored send_command. Added get get_player method

--- app/Controls/controls_controller.rb
+++ app/Controls/controls_controller.rb
@@ -20,34 +20,6 @@ class ControlsController < Rho::RhoController
     end
   end
   
-  def ping_test 
-    if XbmcController.api_loaded?
-      XbmcController::JSONRPC.ping(url_for :action => :ping_callback)
-      render :action => :wait
-    elsif !current_config.nil?
-      XbmcController.load_api(url_for :action => :ping_callback)
-      render :action => :wait
-    end
-  end
-  
-  def ping_callback 
-    puts "#{@params['body']}"
-    if @params['status'] != 'ok'
-      @@test = "Failed"
-      if XbmcController.api_loaded? == false
-        Alert.show_popup ({
-          :message => XbmcController.error[:msg],
-          :title => XbmcController.error[:error],
-          :buttons => ["Close"]
-        })
-      end
-      WebView.navigate(url_for :action => :update_screen)  
-    else
-      @@test = "SUCCESS"
-      WebView.navigate(url_for :action => :update_screen)
-    end
-  end
-  
   def update_screen
     render :action => :index
   end
@@ -62,9 +34,58 @@ class ControlsController < Rho::RhoController
 
   def cancel
     Rho::AsyncHttp.cancel(url_for(:action => :ping_callback) )
-
     @@test  = 'Request was cancelled.'
     render :action => :index
+  end
+  
+  # Sends a command to the server if the API is loaded if not loads the API.
+  # Call example - send_command {XbmcController::JSONRPC.ping (url_for callback)}
+  # Can put any callback method in there.
+  def send_command
+    if XbmcController.api_loaded?
+      yield
+      render :action => :wait
+    elsif !current_config.nil?
+      XbmcController.load_api(url_for :action => :ping_callback)
+      render :action => :wait
+    end
+  end
+  
+  # Handles the callback from sending a command. Will show error message if something
+  # has gone wrong. Handles what to do with results depending on the Params of the
+  # response. 
+  def ping_callback 
+    puts "#{@params['body']}"
+    if @params['status'] != 'ok'
+      @@test = "Failed"
+      if XbmcController.api_loaded? == false
+        Alert.show_popup ({
+          :message => XbmcController.error[:msg],
+          :title => XbmcController.error[:error],
+          :buttons => ["Close"]
+        })
+      end
+      WebView.navigate(url_for :action => :update_screen)  
+    else
+      if @params['method'] == 'player'
+        @@test = @params['body'].with_indifferent_access[:result]
+      else
+        @@test = @params['method']
+      end
+      WebView.navigate(url_for :action => :update_screen)
+    end
+  end
+  
+  # Example method that uses the send_command method. Need to supply :query param to
+  # know what command was sent in the the callback function.
+  def ping_test 
+    send_command {XbmcController::JSONRPC.ping(url_for :action => :ping_callback, :query => {:method => "ping"})}
+  end
+  
+  # Used to find out what the current player is. Will be needed before using controls
+  # This is needed for XBMC Version 10.1
+  def get_player
+    XbmcController::Player.get_active_players(url_for :action => :ping_callback, :query => {:method => "player"})
   end
   
 end
