fe2a86de1d8b009194b31ded4ded4738384296e2
Richard Race <rcr8@aber.ac.uk>
2011-12-25 18:45:11 +0000

    added save after Http removed from address

--- app/helpers/xbmc_config_helper.rb
+++ app/helpers/xbmc_config_helper.rb
@@ -3,7 +3,14 @@ module XbmcConfigHelper
   def current_config
     xbmc_config = XbmcConfig.find(:first, :conditions => {:active => true})
     unless xbmc_config.nil? 
-      XbmcController.setup(xbmc_config.url, xbmc_config.port, xbmc_config.usrname, xbmc_config.password)
+      XbmcConnect.setup(xbmc_config.url, xbmc_config.port, xbmc_config.usrname, xbmc_config.password)
+      xbmc_config.save
+    else 
+      Alert.show_popup ({
+        :message => "Please create or activate an XBMC Config",
+        :title => "No Active XBMC Config",
+        :buttons => ["Close"]
+      })
     end
     return xbmc_config
   end
