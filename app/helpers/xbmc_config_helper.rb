cc2b3df9bfe990032a1ea85d3c06eefb7e05f762
Richard Race <rcr8@aber.ac.uk>
2011-12-27 18:58:05 +0000

    Removed Error message

--- app/helpers/xbmc_config_helper.rb
+++ app/helpers/xbmc_config_helper.rb
@@ -1,18 +1,16 @@
 module XbmcConfigHelper
   
+  class << self
+  
   def current_config
     xbmc_config = XbmcConfig.find(:first, :conditions => {:active => true})
+    
     unless xbmc_config.nil? 
       XbmcConnect.setup(xbmc_config.url, xbmc_config.port, xbmc_config.usrname, xbmc_config.password)
       xbmc_config.save
-    else 
-      Alert.show_popup ({
-        :message => "Please create or activate an XBMC Config",
-        :title => "No Active XBMC Config",
-        :buttons => ["Close"]
-      })
     end
+    
     return xbmc_config
   end
-  
+end
 end
