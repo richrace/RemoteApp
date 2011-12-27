caf36f89b50cbd758978b15f396c4a34ee3ee0a1
Richard Race <rcr8@aber.ac.uk>
2011-12-27 18:58:44 +0000

    V2 Namespace for the JSONRPC API V2

---
+++ app/helpers/xbmc/apis/api_2/api2.rb
@@ -0,0 +1,19 @@
+
+# Adds namespaces in the following format "Api::V2::"
+module ApiV2
+  
+  # Version of which JSONRPC API this reflects
+  VERSION = 2
+  
+  # Holds the commands for VideoPlayer, AudioPlayer and PicturePlayer
+  # Is accessed by "Api::V2::Playback.<command>" The commands are found in
+  # the file listed in the require.
+  class Playback
+    require 'helpers/xbmc/apis/api_2/api2_controls'
+
+    class << self
+      include Control2
+    end
+  end
+  
+end
