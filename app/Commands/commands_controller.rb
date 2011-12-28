f0354bdbab05e350ad99503e3325a03c68cb7366
Richard Race <rcr8@aber.ac.uk>
2011-12-28 11:53:13 +0000

    Test scan command

--- app/Commands/commands_controller.rb
+++ app/Commands/commands_controller.rb
@@ -1,6 +1,7 @@
 require 'rho/rhocontroller'
 require 'helpers/application_helper'
 require 'helpers/browser_helper'
+require 'helpers/xbmc/apis/xbmc_apis'
 
 
 class CommandsController < Rho::RhoController
@@ -9,6 +10,14 @@ class CommandsController < Rho::RhoController
   
   def index
     render
+  end
+  
+  def test_callback
+    puts "IT WORKS"
+  end
+  
+  def scan_video
+    Api::V4::VideoLibrary.scan(url_for :action => :test_callback)
   end
   
 end
