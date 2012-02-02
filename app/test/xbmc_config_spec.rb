74470a7712b80c84564402cc6e1f5fda3e9de9d4
Richard Race <rcr8@aber.ac.uk>
2012-02-02 20:19:45 +0000

    Fixed database adds/deleted

--- app/test/xbmc_config_spec.rb
+++ app/test/xbmc_config_spec.rb
@@ -1,46 +1,61 @@
 describe "XbmcConfig" do
   
+  before(:all) do
+    Rhom::Rhom.database_full_reset
+  end
+  
+  after :each do
+    @xbmc.destroy 
+  end
+
   it "should save with normal data" do
-    xbmc = XbmcConfig.new :name => "Test", :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
+    @xbmc = XbmcConfig.new :name => "Test", :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
     
-    xbmc.save == true
+    @xbmc.save == true
   end
 
   it "should be active" do
-    xbmc = XbmcConfig.new :name => "Test", :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
-    xbmc.save
+    @xbmc = XbmcConfig.new :name => "Test", :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
+    @xbmc.save
     
-    xbmc.is_active?.should == true
+    @xbmc.is_active?.should == true
+  end
+  
+  it "should not be active" do
+    @xbmc = XbmcConfig.new :name => "Test", :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "false"
+    @xbmc.save
+    
+    @xbmc.is_active?.should == false
   end
   
   it "should fail with no name" do
-    xbmc = XbmcConfig.new :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
+    @xbmc = XbmcConfig.new :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
     
-    xbmc.valid?.should == false
+    @xbmc.valid?.should == false
   end
   
   it "should fail with no url" do
-    xbmc = XbmcConfig.new :name => "Test", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
+    @xbmc = XbmcConfig.new :name => "Test", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true"
     
-    xbmc.valid?.should == false
+    @xbmc.valid?.should == false
   end
   
   it "should fail with no port" do
-    xbmc = XbmcConfig.new :url => "localhost", :name => "test", :usrname => "xbmc", :password => "xbmc", :active => "true"
+    @xbmc = XbmcConfig.new :url => "localhost", :name => "test", :usrname => "xbmc", :password => "xbmc", :active => "true"
     
-    xbmc.valid?.should == false
+    @xbmc.valid?.should == false
   end
   
   it "should fail with no username" do
-    xbmc = XbmcConfig.new :url => "localhost", :port => 8080, :name => "test", :password => "xbmc", :active => "true"
+    @xbmc = XbmcConfig.new :url => "localhost", :port => 8080, :name => "test", :password => "xbmc", :active => "true"
     
-    xbmc.valid?.should == false
+    @xbmc.valid?.should == false
   end
   
   it "should fail with text in port" do
-    xbmc = XbmcConfig.new :name => "test", :url => "localhost", :port => "NOTNUM", :usrname => "xbmc", :password => "xbmc", :active => "true"
+    @xbmc = XbmcConfig.new :name => "test", :url => "localhost", :port => "NOTNUM", :usrname => "xbmc", :password => "xbmc", :active => "true"
     
-    xbmc.valid?.should == false
+    @xbmc.valid?.should == false
   end
   
 end
