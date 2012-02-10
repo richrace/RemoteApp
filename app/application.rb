124f128f3235b221a03679f2b07f7307740604b3
Richard Race <rcr8@aber.ac.uk>
2012-02-10 16:19:48 +0000

    Removed commented code for NavBar which is no longer used.

--- app/application.rb
+++ app/application.rb
@@ -8,15 +8,7 @@ class AppApplication < Rho::RhoApplication
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
     # Super must be called *after* settings @tabs!
     @tabs = nil
-=begin    [
-      {:label => "Controller", :action => '/app/Controls'},
-      {:label => "Movies", :action => '/app', :icon => '/public/images/icons/clapboard.png'},
-      {:label => "TV Shows", :action => '/app', :icon => '/public/images/icons/tv-1.png'},
-      {:label => "Playlist", :action => '/app', :icon => '/public/images/icons/movie-1.png'},
-      {:label => "Music", :action => '/app', :icon => '/public/images/icons/note-2.png'},
-      {:label => "Files", :action => '/app', :icon => '/public/images/icons/cabinet.png'}
-    ]
-=end
+
     #To remove default toolbar uncomment next line:
     @@toolbar = nil
     super
@@ -24,5 +16,7 @@ class AppApplication < Rho::RhoApplication
     # Uncomment to set sync notification callback to /app/Settings/sync_notify.
     # SyncEngine::set_objectnotify_url("/app/Settings/sync_notify")
     # SyncEngine.set_notification(-1, "/app/Settings/sync_notify", '')
+     
   end
+
 end
