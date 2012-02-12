696ad77b091726012693594eeeee3ba4d58654a7
Richard Race <rcr8@aber.ac.uk>
2012-02-12 18:39:50 +0000

    Added methods for getting thumbnails for movies
    This needs to be refactored to make it clearer and easier to read.

--- app/Movie/movie_controller.rb
+++ app/Movie/movie_controller.rb
@@ -59,13 +59,13 @@ class MovieController < Rho::RhoController
   # POST /Movie/{1}/delete
   def delete
     @movie = Movie.find(@params['id'])
-    @movie.destroy if @movie
+    if @movie
+      @movie.destroy_image
+      @movie.destroy
+    end
     redirect :action => :index  
   end
   
-  # Updates the AJAX list. If the API hasn't been loaded will attempt to load
-  # the API. Will update list if the API is loaded within the Timeout limit
-  # (10 secs). Spawns a new thread to make sure the UI doesn't freeze.
   def update_list
     @movies = filter_movie_xbmc
     unless @movies.blank?
@@ -80,7 +80,60 @@ class MovieController < Rho::RhoController
       error_handle(@params)
     else
       if sync_movies(@params['body'].with_indifferent_access[:result][:movies])
-        WebView.execute_js("updateList(#{JSON.generate(filter_movie_xbmc)});")
+        @movies = filter_movie_xbmc
+        unless @movies.blank?
+          WebView.execute_js("updateList(#{JSON.generate(@movies)});")
+        end
+      end
+    end
+  end
+  
+  def get_thumb
+    set_callbacks
+    found_movie = find_movie(@params['movieid'])
+    unless found_movie.blank?
+      if found_movie.l_thumb.blank?
+        send_command {Api::V4::VideoLibrary.get_movie_thumb(@thumbnail_cb, @params['movieid'])}
+      else
+        WebView.execute_js("addThumb(#{found_movie.xlib_id},\'#{found_movie.l_thumb}\');")
+      end
+    end
+  end
+  
+  def thumb_cb
+    if @params['status'] != 'ok'
+      error_handle(@params)
+    else
+      @movie_details = @params['body'].with_indifferent_access[:result][:moviedetails]
+      found_movie = find_movie(@movie_details[:movieid])
+      unless found_movie.blank?
+        found_movie.thumb = @movie_details[:thumbnail]
+        found_movie.save
+        url = XbmcConnect::Files.prepare_download(XbmcConnect::NOCALLB, {:path => found_movie.thumb})
+        if url['status'] == 'ok'
+          unless url['body'].with_indifferent_access[:error]
+            xbmc = XbmcConfigHelper.current_config
+            unless xbmc.bank?
+              file = File.join(Rho::RhoApplication::get_base_app_path(), "movie.#{xbmc.object}.#{found_movie.xlib_id}.jpg")
+              found_movie.l_thumb = file
+              found_movie.save
+              params = found_movie.xlib_id
+              set_callbacks
+              XbmcConnect.download_file(url['body'].with_indifferent_access[:result][:details][:path], file, @load_thumb_cb, params)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  def load_thumb_cb
+    if @params['status'] != 'ok'
+      error_handle(@params)
+    else
+      found_movie = find_movie(@params['movieid'])
+      unless found_movie.blank?
+        WebView.execute_js("addThumb(#{found_movie.xlib_id},\'#{found_movie.l_thumb}\');")
       end
     end
   end
