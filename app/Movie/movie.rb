0f893e5bc72e3c2d2f5e5d7e96ce549f7db61f41
Richard Race <rcr8@aber.ac.uk>
2012-02-12 18:39:16 +0000

    Added method that deletes the thumbnail associated with the Movie

--- app/Movie/movie.rb
+++ app/Movie/movie.rb
@@ -4,9 +4,14 @@ class Movie
   include Rhom::PropertyBag
   
   belongs_to :xbmc_id, 'XbmcConfig'
-  
+    
   # Uncomment the following line to enable sync with Movie.
   # enable :sync
 
   #add model specifc code here
+  
+  def destroy_image
+    File.delete(self.l_thumb) if File.exists?(self.l_thumb) 
+  end
+  
 end
