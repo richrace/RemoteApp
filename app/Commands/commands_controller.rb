df740d4524faa3c83b02615425bdfe57aac51c76
Richard Race <rcr8@aber.ac.uk>
2012-02-14 00:41:24 +0000

    Added Input Controls

--- app/Commands/commands_controller.rb
+++ app/Commands/commands_controller.rb
@@ -4,6 +4,7 @@ require 'helpers/browser_helper'
 require 'helpers/commands_helper'
 require 'helpers/error_helper'
 require 'helpers/method_helper'
+require 'helpers/xbmc_config_helper'
 
 class CommandsController < Rho::RhoController
   include ApplicationHelper
@@ -11,6 +12,7 @@ class CommandsController < Rho::RhoController
   include CommandsHelper
   include ErrorHelper
   include MethodHelper
+  include XbmcConfigHelper
   
   def index
     @callback = url_for :action => :cmd_callback
@@ -27,4 +29,35 @@ class CommandsController < Rho::RhoController
     send_command {scan_video_lib(@callback)}
   end
   
+  def clean_video
+    send_command {clean_video_lib(@callback)}
+  end
+  
+  def input_up
+    send_command {xbmc_input_up(@callback)}
+  end
+  
+  def input_left
+    send_command {xbmc_input_left(@callback)}
+  end
+  
+  def input_right
+    send_command {xbmc_input_right(@callback)}
+  end
+  
+  def input_down
+    send_command {xbmc_input_down(@callback)}
+  end
+  
+  def input_select
+    send_command {xbmc_input_select(@callback)}
+  end
+  
+  def input_back
+    send_command {xbmc_input_back(@callback)}
+  end
+  
+  def input_home
+    send_command {xbmc_input_home(@callback)}
+  end
 end
