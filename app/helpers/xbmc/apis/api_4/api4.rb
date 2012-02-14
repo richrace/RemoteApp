ba0e47047664739e722f11aba95d18ea899f490a
Richard Race <rcr8@aber.ac.uk>
2012-02-14 01:42:36 +0000

    Added input support

--- app/helpers/xbmc/apis/api_4/api4.rb
+++ app/helpers/xbmc/apis/api_4/api4.rb
@@ -19,5 +19,20 @@ module ApiV4
     end
   end
   
+  class Input
+    require 'helpers/xbmc/apis/api_4/api4_input'
+
+    class << self
+      include Input4 
+    end
+  end
+
+  class Application
+    require 'helpers/xbmc/apis/api_4/api4_application'
+
+    class << self
+      include Application4
+    end
+  end
 end
  
