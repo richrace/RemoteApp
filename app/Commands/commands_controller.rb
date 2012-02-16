538c5857beffad8992af16313a4bfb8decd2aacb
Richard Race <rcr8@aber.ac.uk>
2012-02-16 11:09:40 +0000

    Added volume options
    Can increase/decrease audio by a set amount of 5 from the current
    volume level.

--- app/Commands/commands_controller.rb
+++ app/Commands/commands_controller.rb
@@ -5,6 +5,7 @@ require 'helpers/commands_helper'
 require 'helpers/error_helper'
 require 'helpers/method_helper'
 require 'helpers/xbmc_config_helper'
+require 'helpers/xbmc_app_helper'
 
 class CommandsController < Rho::RhoController
   include ApplicationHelper
@@ -13,9 +14,9 @@ class CommandsController < Rho::RhoController
   include ErrorHelper
   include MethodHelper
   include XbmcConfigHelper
+  include XbmcAppHelper
   
   def index
-    @callback = url_for :action => :cmd_callback
     render
   end
   
@@ -26,38 +27,93 @@ class CommandsController < Rho::RhoController
   end
   
   def scan_video
-    send_command {scan_video_lib(@callback)}
+    set_callbacks
+    send_command {scan_video_lib(@cmd_callback)}
   end
   
   def clean_video
-    send_command {clean_video_lib(@callback)}
+    set_callbacks
+    send_command {clean_video_lib(@cmd_callback)}
   end
   
   def input_up
-    send_command {xbmc_input_up(@callback)}
+    set_callbacks
+    send_command {xbmc_input_up(@cmd_callback)}
   end
   
   def input_left
-    send_command {xbmc_input_left(@callback)}
+    set_callbacks
+    send_command {xbmc_input_left(@cmd_callback)}
   end
   
   def input_right
-    send_command {xbmc_input_right(@callback)}
+    set_callbacks
+    send_command {xbmc_input_right(@cmd_callback)}
   end
   
   def input_down
-    send_command {xbmc_input_down(@callback)}
+    set_callbacks
+    send_command {xbmc_input_down(@cmd_callback)}
   end
   
   def input_select
-    send_command {xbmc_input_select(@callback)}
+    set_callbacks
+    send_command {xbmc_input_select(@cmd_callback)}
   end
   
   def input_back
-    send_command {xbmc_input_back(@callback)}
+    set_callbacks
+    send_command {xbmc_input_back(@cmd_callback)}
   end
   
   def input_home
-    send_command {xbmc_input_home(@callback)}
+    set_callbacks
+    send_command {xbmc_input_home(@cmd_callback)}
+  end
+  
+  def toggle_mute
+    set_callbacks
+    send_command {xbmc_toggle_mute(@mute_callback)}
+  end
+  
+  def increase_volume
+    set_callbacks
+    send_command {xbmc_increase_volume(@change_vol_cb)}
+  end
+  
+  def decrease_volume
+    set_callbacks
+    send_command {xbmc_decrease_volume(@change_vol_cb)}
+  end
+  
+  def load_volume
+    set_callbacks
+    send_command {xbmc_get_volume(@get_vol_cb)}
+  end
+  
+  def change_vol_callback
+    if @params['status'] == 'ok'
+      cur_vol = @params['body'].with_indifferent_access[:result]
+      WebView.execute_js("updateVolume('#{cur_vol}');")
+    end
+  end
+  
+  def get_vol_callback
+    if @params['status'] == 'ok'
+      cur_vol = @params['body'].with_indifferent_access[:result][:volume]
+      WebView.execute_js("updateVolume('#{cur_vol}');")
+    end
+  end
+  
+  def mute_callback
+    if @params['status'] == 'ok'
+      result = @params['body'].with_indifferent_access[:result]
+      if result
+        cur_vol = "Muted"
+        WebView.execute_js("updateVolume('#{cur_vol}');")
+      else
+        load_volume
+      end
+    end
   end
 end
