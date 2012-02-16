1a25aab1d97c15606cd255d95a24a7d2b44f6c8a
Richard Race <rcr8@aber.ac.uk>
2012-02-16 23:01:52 +0000

    Updated to no longer use get_movie_details

--- app/helpers/movie_helper.rb
+++ app/helpers/movie_helper.rb
@@ -1,9 +1,11 @@
 require 'helpers/movie_helper'
 require 'helpers/method_helper'
+require 'helpers/sort_helper'
 
 module MovieHelper
   include XbmcConfigHelper
   include MethodHelper
+  include SortHelper
   
   def set_callbacks
     @movies_cb = url_for(:action => :movies_callback)
@@ -25,10 +27,6 @@ module MovieHelper
     Movie.find(:first, :conditions => {:xbmc_id => XbmcConfigHelper.current_config.object, :xlib_id => xbmc_lib_id}) 
   end
   
-  def create_sort_title(title)
-    title.sub(/^(the|a|an)\s+/i,'')
-  end
-  
   def sync_movies(movies)
     handle_new_movies(movies) || handle_removed_movies(movies)
   end
@@ -40,11 +38,37 @@ module MovieHelper
       if found.blank?
         t_movie = Movie.new :xbmc_id => XbmcConfigHelper.current_config.object, :xlib_id => new_movie[:movieid],  :label => new_movie[:label]
         t_movie.url = url_for(:action => :show, :id => t_movie.object)
-        t_movie.sorttitle = create_sort_title(t_movie.label)
+        
+        t_movie.thumb = new_movie[:thumbnail]
+        t_movie.fanart = new_movie[:fanart]
+        t_movie.imdbnumber = new_movie[:imdbnumber]
+        t_movie.trailer = new_movie[:trailer].scan(/videoid\=+(.+)$/).flatten[0]
+        t_movie.plot = new_movie[:plot]
+        t_movie.rating = new_movie[:rating]
+        t_movie.genre = new_movie[:genre]
+        t_movie.year = new_movie[:year]
+        t_movie.playcount = new_movie[:playcount]
+        t_movie.studio = new_movie[:studio]
+        t_movie.title = new_movie[:title]
+        
+        t_movie.sorttitle = create_sort_title(t_movie.title)
         t_movie.save
-        set_callbacks
-        send_command {Api::V4::VideoLibrary.get_movie_details(@movie_details_cb, t_movie.xlib_id)}
         list_changed = true
+                
+        url = XbmcConnect::Files.prepare_download(XbmcConnect::NOCALLB, {:path => t_movie.thumb})
+        if url['status'] == 'ok'
+          unless url['body'].with_indifferent_access[:error]
+            xbmc = XbmcConfigHelper.current_config
+            unless xbmc.bank?
+              file = File.join(Rho::RhoApplication::get_base_app_path(), "#{xbmc.object}.movies.#{t_movie.xlib_id}.jpg")
+              t_movie.l_thumb = file
+              t_movie.save
+              params = "movieid=#{t_movie.xlib_id}"
+              set_callbacks
+              XbmcConnect.download_file(url['body'].with_indifferent_access[:result][:details][:path], file, @load_thumb_cb, params)
+            end
+          end
+        end
       end
     end
     return list_changed
