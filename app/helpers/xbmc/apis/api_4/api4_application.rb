1802e998ee40aa237413f61f6ef20f6944880af0
Richard Race <rcr8@aber.ac.uk>
2012-02-16 11:13:00 +0000

    Added volume functions
    Can now toggle mute on/off.
    Increase/decrease current volume by 5.
    Also can get current volume.

--- app/helpers/xbmc/apis/api_4/api4_application.rb
+++ app/helpers/xbmc/apis/api_4/api4_application.rb
@@ -1,5 +1,10 @@
 module Application4
   
+  def toggle_mute(callback)
+    params = {:mute => 'toggle'}
+    XbmcConnect::Application.set_mute(callback, params)
+  end
+  
   def set_mute(callback, bool)
     params = {:mute => bool}
     XbmcConnect::Application.set_mute(callback, params)
@@ -8,5 +13,34 @@ module Application4
   def set_volume(callback, vol) 
     params = {:volume => vol.to_i}
     XbmcConnect::Application.set_volume(callback, params)
+  end
+  
+  def increase_volume(callback) 
+    cur_vol = get_volume(XbmcConnect::NOCALLB)['body'].with_indifferent_access[:result][:volume]
+    if (cur_vol == 100)
+      return
+    elsif (cur_vol > 96)
+      set_vol = 100
+    else
+      set_vol = cur_vol + 5
+    end
+    set_volume(callback, set_vol)
+  end
+  
+  def decrease_volume(callback)
+    cur_vol = get_volume(XbmcConnect::NOCALLB)['body'].with_indifferent_access[:result][:volume]
+    if (cur_vol == 0)
+      return
+    elsif (cur_vol < 5)
+      set_vol = 0
+    else
+      set_vol = cur_vol - 5
+    end
+    set_volume(callback, set_vol)
+  end
+  
+  def get_volume(callback) 
+    params = {:properties => ['volume']}
+    XbmcConnect::Application.get_properties(callback, params)
   end
 end
