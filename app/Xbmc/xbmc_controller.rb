abc4b2d71a4a51dcb5403af9aafaa7aa4f8c0d7c
Richard Race <rcr8@aber.ac.uk>
2012-02-24 00:23:09 +0000

    Updated for the new Alert boxes from errors.

--- app/Xbmc/xbmc_controller.rb
+++ app/Xbmc/xbmc_controller.rb
@@ -4,20 +4,32 @@ require 'helpers/ruby_ext'
 require 'helpers/browser_helper'
 require 'helpers/xbmc/xbmc_connect'
 require 'json'
+require 'helpers/error_helper'
 
 # A simple XBMC JSON RPC API Client. See README for details.
 class XbmcController < Rho::RhoController
   include ApplicationHelper
   include BrowserHelper
+  include ErrorHelper
   
   # Returns an array of available api commands instantiated as Xbmc::Command objects
   def commands
-    puts "**** LOADING COMMANDS ****"
-    XbmcConnect.load_commands(@params)
+    puts "**** COMMANDS CALLBACK ****"
+    if @params['status'] == 'ok'
+      XbmcConnect.load_commands(@params)
+    else
+      error_handle(@params)
+      WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
+    end
   end
   
   def version
-    puts "**** LOADING VERSION ****"
-    XbmcConnect.load_version(@params)
+    puts "**** VERSION CALLBACK ****"
+    if @params['status'] == 'ok'
+      XbmcConnect.load_version(@params)
+    else
+      error_handle(@params)
+      WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
+    end
   end
 end
