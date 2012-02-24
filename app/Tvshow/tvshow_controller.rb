f4391a54ad43676af96b9a6e65065b7b16deb02e
Richard Race <rcr8@aber.ac.uk>
2012-02-24 00:22:50 +0000

    Updated for Syncing properly and Sorting options.

--- app/Tvshow/tvshow_controller.rb
+++ app/Tvshow/tvshow_controller.rb
@@ -12,6 +12,12 @@ class TvshowController < Rho::RhoController
   include TvShowHelper
   include ErrorHelper
 
+  @@conditions = {}
+  @@order = {:title => :sorttitle, :year => :year, :rating => :rating}
+  @@active_order = :sorttitle
+  @@order_dir = 'ASC'
+  @@watch_list = false 
+
   # GET /Tvshow
   def index
     render
@@ -33,8 +39,9 @@ class TvshowController < Rho::RhoController
   end
   
   def get_tv_shows
-    @tvshows = filter_tvshow_xbmc
+    @tvshows = filter_tvshows_xbmc(@@conditions, @@active_order, @@order_dir)
     unless @tvshows.blank?
+      ensure_sorted(@tvshows)
       WebView.execute_js("updateTVList(#{JSON.generate(@tvshows)});")
     end
     send_command {load_tv_shows(url_for :action => :tv_shows_callback)}
@@ -43,29 +50,84 @@ class TvshowController < Rho::RhoController
   def tv_shows_callback
     if @params['status'] == 'ok'
       if sync_tv_shows(@params['body'].with_indifferent_access[:result][:tvshows])
-        @tvshows = filter_tvshow_xbmc
+        @tvshows = filter_tvshows_xbmc(@@conditions, @@active_order, @@order_dir)
         unless @tvshows.blank?
+          ensure_sorted(@tvshows)
           WebView.execute_js("updateTVList(#{JSON.generate(@tvshows)});")
         end
       end
+    else
+      error_handle(@params)
+      WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
     end
   end
   
   def get_tv_thumb
     found_tvshow = find_tvshow(@params['tvshowid'])
     unless found_tvshow.blank? && found_tvshow.l_thumb.blank?
-              WebView.execute_js("addTVThumb(#{found_tvshow.xlib_id},\'#{found_tvshow.l_thumb}\');")
+      WebView.execute_js("addTVThumb(#{found_tvshow.xlib_id},\'#{found_tvshow.l_thumb}\');")
     end
   end
   
   def thumb_callback
-    if @params['status'] != 'ok'
-      error_handle(@params)
-    else
+    if @params['status'] == 'ok'
       found_tvshow = find_tvshow(@params['tvshowid'])
       unless found_tvshow.blank?
         WebView.execute_js("addTVThumb(#{found_tvshow.xlib_id},\'#{found_tvshow.l_thumb}\');")
       end
+    else
+      error_handle(@params)
+      WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
     end
   end
+
+  def get_watch_list_bool
+    @@watch_list
+  end
+  
+  def get_order_dir
+    @@order_dir
+  end
+
+  def get_order
+    @@order
+  end
+
+  def get_active_order
+    @@active_order
+  end
+  
+  def tvsort
+    if !@params['watch_later'].blank?
+      @@watch_list = @params['watch_later']
+      if @@watch_list == "true"
+        @@conditions = {:watch_later => @@watch_list}
+      else 
+        @@conditions = {}
+      end
+    elsif !@params['order_dir'].blank?
+      @@order_dir = "#{@params['order_dir']}"
+    elsif !@params['order'].blank?
+      wanted = @params['order']
+      @@order.each do | key, value |
+        if wanted == key.to_s
+          @@active_order = value 
+        end
+      end
+    end
+  end 
+
+  # Ensure that the list has been sorted properly.
+  # This is used because Rhom only seems to sort by the first 
+  # number. Therefore, iterate over the list to make sure it has
+  # been sorted. This works for both ASC and DESC lists.
+  def ensure_sorted(tvshows)
+    if @@active_order == @@order[:rating]
+      tvshows.sort_by! {|tvshow| tvshow.rating.to_f}
+      if @@order_dir == 'DESC'
+        tvshows.reverse!
+      end
+    end
+  end
+
 end
