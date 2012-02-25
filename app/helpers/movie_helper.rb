eb92588dc86c8057ba1f7bc08114ff5a900e01f2
Richard Race <rcr8@aber.ac.uk>
2012-02-25 02:42:38 +0000

    Moved downloading of thumbnails added search_by_title method
    Thumbnail downloading is now handled in Download Helper and
    search_by_title searches within using the database SQL.

--- app/helpers/movie_helper.rb
+++ app/helpers/movie_helper.rb
@@ -1,11 +1,13 @@
 require 'helpers/movie_helper'
 require 'helpers/method_helper'
 require 'helpers/sort_helper'
+require 'helpers/download_helper'
 
 module MovieHelper
   include XbmcConfigHelper
   include MethodHelper
   include SortHelper
+  include DownloadHelper
   
   def set_callbacks
     @movies_cb = url_for(:action => :movies_callback)
@@ -33,6 +35,7 @@ module MovieHelper
       return nil
     end
   end
+  
   def find_movie(xbmc_lib_id)
     Movie.find(:first, :conditions => {:xbmc_id => XbmcConfigHelper.current_config.object, :xlib_id => xbmc_lib_id}) 
   end
@@ -47,11 +50,13 @@ module MovieHelper
       found = find_movie(new_movie[:movieid])
       if found.blank?
         t_movie = Movie.new :xbmc_id => XbmcConfigHelper.current_config.object, :xlib_id => new_movie[:movieid],  :label => new_movie[:label]
+        # Makes the url here so can use AJAX call.
         t_movie.url = url_for(:action => :show, :id => t_movie.object)
         
         t_movie.thumb = new_movie[:thumbnail]
         t_movie.fanart = new_movie[:fanart]
         t_movie.imdbnumber = new_movie[:imdbnumber]
+        # Strips the YouTube url from the string.
         t_movie.trailer = new_movie[:trailer].scan(/videoid\=+(.+)$/).flatten[0]
         t_movie.plot = new_movie[:plot]
         t_movie.rating = new_movie[:rating]
@@ -64,21 +69,7 @@ module MovieHelper
         t_movie.sorttitle = create_sort_title(t_movie.title)
         t_movie.save
         list_changed = true
-                
-        url = XbmcConnect::Files.prepare_download(XbmcConnect::NOCALLB, {:path => t_movie.thumb})
-        if url['status'] == 'ok'
-          unless url['body'].with_indifferent_access[:error]
-            xbmc = XbmcConfigHelper.current_config
-            unless xbmc.bank?
-              file = File.join(Rho::RhoApplication::get_base_app_path(), "#{xbmc.object}.movies.#{t_movie.xlib_id}.jpg")
-              t_movie.l_thumb = file
-              t_movie.save
-              params = "movieid=#{t_movie.xlib_id}"
-              set_callbacks
-              XbmcConnect.download_file(url['body'].with_indifferent_access[:result][:details][:path], file, @load_thumb_cb, params)
-            end
-          end
-        end
+        Thread.new {download_moviethumb(t_movie)}
       end
     end
     return list_changed
@@ -103,5 +94,24 @@ module MovieHelper
     end
     return list_changed
   end
+
+  # This method doesn't search for title by using the databse (SQL). 
+  # Instead this gets all the movies in the current XBMC Config and 
+  # sees if the given Movie title is in the wanted_title. This is 
+  # becuase the wanted_title can include other text such as "DVD" or 
+  # "(Comedy)"
+  def search_by_title(wanted_title)
+    movies = get_movies_xbmc
+    found_movies = []
+    unless movies.blank? && wanted_title.blank?
+      movies.each do | movie |
+        if wanted_title.downcase.gsub('-', ' ').include?(movie.title.downcase.gsub('-', ' '))
+          found_movies << movie
+        end
+      end
+    end
+    return found_movies
+  end
+
 end
 
