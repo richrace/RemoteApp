ef8c007cf46a3e9d693533abaff749eea657af7a
Richard Race <rcr8@aber.ac.uk>
2012-02-27 12:56:02 +0000

    Added Native menu for Android.

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,14 @@ class AppApplication < Rho::RhoApplication
     #To remove default toolbar uncomment next line:
     @@toolbar = nil
     super
+    @default_menu = {
+      "Controls" => "/app/Controls", 
+      "Commands" => "/app/Commands",
+      "Scan Barcode" => "app/BarcodeScan",
+      :separator => nil,
+      "Settings" => "app/XbmcConfig",
+      "Close" => :close
+    }
 
     # Uncomment to set sync notification callback to /app/Settings/sync_notify.
     # SyncEngine::set_objectnotify_url("/app/Settings/sync_notify")
