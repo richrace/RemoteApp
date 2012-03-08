8cd0d5589f2f84b95b8ce30ce26272ff3c698dae
Richard Race <rcr8@aber.ac.uk>
2012-03-08 13:02:38 +0000

    Added Product unit seats.

--- app/test/product_spec.rb
+++ app/test/product_spec.rb
@@ -1,7 +1,26 @@
 describe "Product" do
-  #this test always fails, you really should have tests!
+  
+  before(:all) do
+    Rhom::Rhom.database_full_reset
+    @xbmc = XbmcConfig.new(:name => "Test", :url => "localhost", :port => 8080, :usrname => "xbmc", :password => "xbmc", :active => "true")
+    @xbmc.save
+  end
 
-  it "should have tests" do
-    true.should == false
+  it "should have no be valid with no title and with xbmc id" do
+    @product = Product.new(:xbmc_id => @xbmc.object)
+
+    @product.valid?.should == false
+  end
+
+  it "should be valid with title and xbmc id" do
+    @product = Product.new(:title => "Test", :xbmc_id => @xbmc.object)
+
+    @product.valid?.should == true
+  end
+
+  it "should not be valid without title and xbmc id" do
+    @product = Product.new
+
+    @product.valid?.should == false
   end
 end
