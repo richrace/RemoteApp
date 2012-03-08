04fc74a5bf0d8dbebd00d0d327cd91c906ec47be
Richard Race <rcr8@aber.ac.uk>
2012-03-08 13:01:23 +0000

    Added comments for making a sort title, changed name.
    Was create_sort_title, now make_sort_title.

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -1,6 +1,9 @@
 module SortHelper
   
-  def create_sort_title(title)
+  # Strips the leading 'The/An/A' of a given string. Returns the
+  # String without altering the original. This is not case 
+  # sensitive.
+  def make_sort_title(title)
     title.sub(/^(the|a|an)\s+/i,'')
   end
   
