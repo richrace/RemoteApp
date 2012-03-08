0074bf22c39204d7a570979b5766213b0eb5ec68
Richard Race <rcr8@aber.ac.uk>
2012-03-08 13:24:58 +0000

    Now uses FixedSchema, added validation.

--- app/Tvshow/tvshow.rb
+++ app/Tvshow/tvshow.rb
@@ -1,16 +1,63 @@
-# The model has already been created by the framework, and extends Rhom::RhomObject
-# You can add more methods here
+require 'helpers/sort_helper'
+
 class Tvshow
-  include Rhom::PropertyBag
+  include Rhom::FixedSchema
+  include Validatable
+  include SortHelper
+
   belongs_to :xbmc_id, 'XbmcConfig'
 
-  # Uncomment the following line to enable sync with Tvshow.
-  # enable :sync
+  validates_presence_of :xbmc_id, :message => "Needs to have an XBMC Config"
+  validates_presence_of :title, :message => "Requires title"
+  validates_presence_of :xlib_id, :message => "Requires XBMC Movie ID"
+  validates_presence_of :label, :message => "Requires label"
+  validates_presence_of :thumb, :message => "Requires remote thumb"
+  validates_presence_of :fanart, :message => "Requires requires remote fanart"
+  validates_presence_of :tvdb, :message => "Requires requires imdbnumber"
+  validates_presence_of :plot, :message => "Requires plot"
+  validates_presence_of :rating, :message => "Requires rating"
+  validates_presence_of :year, :message => "Requires year"
 
-  #add model specifc code here
+  set :schema_version, '1.0'
+
+  # Required, these must be added to the movie so it is valid
+  property :title, :string
+  property :xlib_id, :integer
+  property :xbmc_id, :float
+  property :label, :string
+  property :thumb, :string
+  property :fanart, :string
+  property :tvdb, :string
+  property :plot, :string
+  property :rating, :float
+  property :year, :integer
+
+  # The following are required but are added at a later date.
+  property :sorttitle, :string
+  property :url, :string
+  # These two fields point to a file stored on the device rather than
+  # storing an image in the database.
+  property :l_thumb, :string
+  property :l_fanart, :string
+
+  # These are not required at the moment but are stored in the database
+  # in case they are needed in future. Therefore, they are not required
+  # to create a Movie object.
+  property :playcount, :integer
+  property :studio, :string
+  property :genre, :string
+
   def destroy_image
     unless self.l_thumb.blank?
       File.delete(self.l_thumb) if File.exists?(self.l_thumb) 
+      self.l_thumb = nil
+      self.save
     end
   end
+
+  def create_sort_title
+    self.sorttitle = make_sort_title(self.title)
+    self.save
+  end
+
 end
