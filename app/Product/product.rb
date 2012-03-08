df941551013bcebfd1bc49a5aab4c3ba79d2cee1
Richard Race <rcr8@aber.ac.uk>
2012-03-08 12:50:06 +0000

    Converted to FixedSchema
    Added properties and validation checks.

--- app/Product/product.rb
+++ app/Product/product.rb
@@ -1,10 +1,22 @@
 # The model has already been created by the framework, and extends Rhom::RhomObject
 # You can add more methods here
 class Product
-  include Rhom::PropertyBag
+  include Rhom::FixedSchema
+  include Validatable
+
+  belongs_to :xbmc_id, 'XbmcConfig'
+
+  validates_presence_of :title, :message => "Product needs to have a title"
+  validates_presence_of :xbmc_id, :message => "Needs to have an XBMC Config"
 
   # Uncomment the following line to enable sync with Product.
   # enable :sync
 
   #add model specifc code here
+
+  set :schema_version, "1.0"
+
+  property :title, :string
+  property :xbmc_id, :float
+
 end
