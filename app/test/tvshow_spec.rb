0ed62b028ce553b5ff821b41322df991486584a5
Richard Race <rcr8@aber.ac.uk>
2012-03-08 13:27:47 +0000

    Created tests for Tvshow.

--- app/test/tvshow_spec.rb
+++ app/test/tvshow_spec.rb
@@ -1,7 +1,26 @@
 describe "Tvshow" do
   #this test always fails, you really should have tests!
 
-  it "should have tests" do
-    true.should == false
+  it "tvshow should be valid with all fields" do
+    @tvshow = Tvshow.new(
+      :xbmc_id => 223,
+      :xlib_id => 222,
+      :title => "Test",
+      :label => "test",
+      :thumb => "test",
+      :fanart => "test",
+      :tvdb => "test",
+      :plot => "test",
+      :rating => 9.9,
+      :year => 200,
+      :sorttitle => "test",
+      :url => "Test",
+      :l_thumb => "test",
+      :l_fanart => "test",
+      :genre => "test",
+      :playcount => 1,
+      :studio => "test")
+
+    @tvshow.valid?.should == true
   end
 end
