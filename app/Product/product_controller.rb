66b07bd0c490ca2eba215e3239f04a817427d70e
Richard Race <rcr8@aber.ac.uk>
2012-03-08 12:56:32 +0000

    Added filter for XBMC configs, updated to handle errors.

--- app/Product/product_controller.rb
+++ app/Product/product_controller.rb
@@ -2,22 +2,26 @@ require 'rho/rhocontroller'
 require 'helpers/application_helper'
 require 'helpers/browser_helper'
 require 'helpers/movie_helper'
+require 'helpers/xbmc_config_helper'
+require 'helpers/product_helper'
 
 class ProductController < Rho::RhoController
   include ApplicationHelper
   include BrowserHelper
   include MovieHelper
+  include XbmcConfigHelper
+  include ProductHelper
 
   # GET /Product
   def index
-    products = Product.find(:all)
+    products = filter_products_xbmc
     products.each do | product |
       movies = search_by_title(product.title)
       unless movies.blank?
         product.destroy
       end
     end
-    @products = Product.find(:all)
+    @products = filter_products_xbmc
     render
   end
 
@@ -41,9 +45,16 @@ class ProductController < Rho::RhoController
   def create
     found_movies = search_by_title(@params['product']['title'])
     if found_movies.blank?
-      @product = Product.new(@params['product'])
-      @product.save
-      redirect :action => :index
+      title = @params['product']['title']
+      xbmc = XbmcConfigHelper.current_config
+      @product = Product.new(:title => title, :xbmc_id => xbmc.object)
+      if @product.valid?
+        @product.save
+        redirect :action => :index
+      else 
+        @errors = @product.errors.to_json
+        render :action => :new
+      end
     else
       if found_movies.length == 1
         redirect(:controller => :Movie, :action => :show, :id => found_movies[0].object)
