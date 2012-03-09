04b9fc28ca9761055eee5647b0105d5bff3949e0
Richard Race <race.richard@gmail.com>
2012-03-09 14:47:42 +0000

    Removed comments, added extra validation checks

--- app/XbmcConfig/xbmc_config.rb
+++ app/XbmcConfig/xbmc_config.rb
@@ -1,5 +1,3 @@
-# The model has already been created by the framework, and extends Rhom::RhomObject
-# You can add more methods here
 class XbmcConfig
   include Rhom::FixedSchema
   include Validatable
@@ -8,24 +6,26 @@ class XbmcConfig
   validates_presence_of :url, :message => "You must include a URL (can be IP or hostname)"
   validates_presence_of :port, :message => "You need to include a port number"
   validates_presence_of :usrname, :message => "You need to include your XBMC username"
-  validates_numericality_of :port, :message => "Port needs to be a number"
-
-  # Uncomment the following line to enable sync with XbmcConfig.
-  # enable :sync
-
-  #add model specifc code here
   
-  # Set the current version of the fixed schema.
-  # Your application may use it for data migrations.
+  validates_numericality_of :port, :message => "Port needs to be a integer", :only_integer => true
+  validates_numericality_of :version, :message => "Version needs to be an integer", :only_integer => true
+
+  # Before Validation is used to assign 0 value to
+  # version if it has not been assigned any value. 
+  before_validation do
+    unless self.version
+      self.version = 0
+    end
+  end
+
   set :schema_version, '1.3'
 
-  # Define fixed schema attributes.
-  # :string and :blob types are supported.
   property :name, :string
   property :url, :string
   property :port, :integer
   property :usrname, :string
   property :password, :string
+
   property :active, :boolean  
   property :version, :integer  
   
