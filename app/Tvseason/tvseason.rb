42d6b0e093665a6a13ffd5b2425bc202a4086a9c
Richard Race <rcr8@aber.ac.uk>
2012-03-09 12:41:19 +0000

    Moved to fixed schema

--- app/Tvseason/tvseason.rb
+++ app/Tvseason/tvseason.rb
@@ -1,17 +1,41 @@
-# The model has already been created by the framework, and extends Rhom::RhomObject
-# You can add more methods here
-class Tvseason
-  include Rhom::PropertyBag
-  belongs_to :tvshow_id, 'Tvshow'
-  belongs_to :xbmc_id, 'XbmcConfig'
-  
-  # Uncomment the following line to enable sync with Tvseason.
-  # enable :sync
+require 'helpers/sort_helper'
 
-  #add model specifc code here
+class Tvseason
+  include Rhom::FixedSchema
+  include Validatable
+  include SortHelper  
+
+  belongs_to :tvshow_id, 'Tvshow'  
+  belongs_to :xbmc_id, 'XbmcConfig'
+
+  validates_presence_of :xbmc_id, :message => "Needs to have an XBMC Config"
+  validates_presence_of :xlib_id, :message => "Requires XBMC Season ID"
+  validates_presence_of :tvshow_id, :message => "Requires XBMC TV Show ID"
+  validates_presence_of :label, :message => "Requires label"
+  validates_presence_of :showtitle, :message => "Requires tv show title"
+  validates_presence_of :thumb, :message => "Requires remote thumb"
+  validates_presence_of :fanart, :message => "Requires requires remote fanart"
+
+  set :schema_version, '1.0'
+
+  property :xbmc_id, :float
+  property :xlib_id, :integer
+  property :tvshow_id, :integer
+  property :label, :string
+  property :showtitle, :string
+  property :thumb, :string
+  property :fanart, :string
+
+  property :l_thumb, :string
+  property :l_fanart, :string
+  property :url, :string
+  
+  
   def destroy_image
     unless self.l_thumb.blank?
       File.delete(self.l_thumb) if File.exists?(self.l_thumb) 
+      self.l_thumb = nil
+      self.save
     end
   end
 end
