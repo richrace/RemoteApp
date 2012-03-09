565f5208f0903a5488c947dc9b6821e193cc0ed9
Richard Race <rcr8@aber.ac.uk>
2012-03-09 13:36:20 +0000

    Added unit test.

--- app/test/tvepisode_spec.rb
+++ app/test/tvepisode_spec.rb
@@ -1,7 +1,25 @@
 describe "Tvepisode" do
-  #this test always fails, you really should have tests!
 
-  it "should have tests" do
-    true.should == false
+  it "tvepisode should be valid with all fields" do
+    @episode = Tvepisode.new(
+      :xbmc_id => 223,
+      :xlib_id => 222,
+      :episode => 2,
+      :firstaired => "2/3/2001",
+      :tvshow_id => 321,
+      :tvshow_name => "Test",
+      :tvseason_id => 1,
+      :runtime => "test",
+      :title => "Test",
+      :label => "test",
+      :thumb => "test",
+      :fanart => "test",
+      :plot => "test",
+      :rating => 9.9,
+      :url => "Test",
+      :l_thumb => "test",
+      :l_fanart => "test")
+
+    @episode.valid?.should == true
   end
 end
