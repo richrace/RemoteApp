55075a736a2ee3a13fff729e16c1724e9ebe3326
Richard Race <rcr8@aber.ac.uk>
2012-03-09 13:36:26 +0000

    Added unit test.

--- app/test/tvseason_spec.rb
+++ app/test/tvseason_spec.rb
@@ -1,7 +1,19 @@
 describe "Tvseason" do
   #this test always fails, you really should have tests!
 
-  it "should have tests" do
-    true.should == false
+  it "tvseason should be valid with all fields" do
+    @season = Tvseason.new(
+      :xbmc_id => 213,
+      :xlib_id => 2,
+      :tvshow_id => 2, 
+      :label => "test",
+      :showtitle => "Test",
+      :thumb => "Test",
+      :fanart => "test",
+      :l_thumb => "Test",
+      :l_fanart => "test",
+      :url => "test")
+
+    @season.valid?.should == true
   end
 end
