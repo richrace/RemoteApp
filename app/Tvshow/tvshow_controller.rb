e94633dbf000523ed73558380d0c018b6db9f4a4
Richard Race <race.richard@gmail.com>
2012-03-11 13:39:18 +0000

    Removed unneeded sorting method
    Added validation in the database now fixes the sorting of numbers
    correctly.

--- app/Tvshow/tvshow_controller.rb
+++ app/Tvshow/tvshow_controller.rb
@@ -18,7 +18,6 @@ class TvshowController < Rho::RhoController
   @@order = {:title => :sorttitle, :year => :year, :rating => :rating}
   @@active_order = :sorttitle
   @@order_dir = 'ASC'
-  @@watch_list = false 
 
   # GET /Tvshow
   def index
@@ -44,7 +43,6 @@ class TvshowController < Rho::RhoController
     WebView.execute_js("showLoading('Loading TV Shows');")
     @tvshows = filter_tvshows_xbmc(@@conditions, @@active_order, @@order_dir)
     unless @tvshows.blank?
-      ensure_sorted(@tvshows)
       WebView.execute_js("updateTVList(#{JSON.generate(@tvshows)});")
     end
     send_command {load_tv_shows(url_for :action => :tv_shows_callback)}
@@ -55,10 +53,10 @@ class TvshowController < Rho::RhoController
       if sync_tv_shows(@params['body'].with_indifferent_access[:result][:tvshows])
         @tvshows = filter_tvshows_xbmc(@@conditions, @@active_order, @@order_dir)
         unless @tvshows.blank?
-          ensure_sorted(@tvshows)
           WebView.execute_js("updateTVList(#{JSON.generate(@tvshows)});")
         end
       end
+      update_seasons
     else
       error_handle(@params)      
       WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
@@ -92,10 +90,6 @@ class TvshowController < Rho::RhoController
       #WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
     end
   end
-
-  def get_watch_list_bool
-    @@watch_list
-  end
   
   def get_order_dir
     @@order_dir
@@ -110,14 +104,7 @@ class TvshowController < Rho::RhoController
   end
   
   def tvsort
-    if !@params['watch_later'].blank?
-      @@watch_list = @params['watch_later']
-      if @@watch_list == "true"
-        @@conditions = {:watch_later => @@watch_list}
-      else 
-        @@conditions = {}
-      end
-    elsif !@params['order_dir'].blank?
+    if !@params['order_dir'].blank?
       @@order_dir = "#{@params['order_dir']}"
     elsif !@params['order'].blank?
       wanted = @params['order']
@@ -128,18 +115,5 @@ class TvshowController < Rho::RhoController
       end
     end
   end 
-
-  # Ensure that the list has been sorted properly.
-  # This is used because Rhom only seems to sort by the first 
-  # number. Therefore, iterate over the list to make sure it has
-  # been sorted. This works for both ASC and DESC lists.
-  def ensure_sorted(tvshows)
-    if @@active_order == @@order[:rating]
-      tvshows.sort_by! {|tvshow| tvshow.rating.to_f}
-      if @@order_dir == 'DESC'
-        tvshows.reverse!
-      end
-    end
-  end
 
 end
