54f99812bd0c31e4a1a1112919adc16d09df8f4f
Richard Race <race.richard@gmail.com>
2012-04-26 21:53:25 +0100

    Added comments. Fixed methods.

--- app/Tvepisode/tvepisode_controller.rb
+++ app/Tvepisode/tvepisode_controller.rb
@@ -1,3 +1,7 @@
+# Author::    Richard Race (rcr8)
+# Copyright:: Copyright (c) 2012
+# License::   MIT Licence
+
 require 'rho/rhocontroller'
 require 'helpers/browser_helper'
 require 'helpers/tv_episode_helper'
@@ -5,6 +9,7 @@ require 'helpers/method_helper'
 require 'helpers/download_helper'
 require 'helpers/error_helper'
 
+# Controller for the TV Episodes.
 class TvepisodeController < Rho::RhoController
   include BrowserHelper
   include TvEpisodeHelper
@@ -29,35 +34,6 @@ class TvepisodeController < Rho::RhoController
     end
   end
 
-  # GET /Tvepisode/new
-  def new
-    @tvepisode = Tvepisode.new
-    render :action => :new, :back => url_for(:action => :index)
-  end
-
-  # GET /Tvepisode/{1}/edit
-  def edit
-    @tvepisode = Tvepisode.find(@params['id'])
-    if @tvepisode
-      render :action => :edit, :back => url_for(:action => :index)
-    else
-      redirect :action => :index
-    end
-  end
-
-  # POST /Tvepisode/create
-  def create
-    @tvepisode = Tvepisode.create(@params['tvepisode'])
-    redirect :action => :index
-  end
-
-  # POST /Tvepisode/{1}/update
-  def update
-    @tvepisode = Tvepisode.find(@params['id'])
-    @tvepisode.update_attributes(@params['tvepisode']) if @tvepisode
-    redirect :action => :index
-  end
-
   # POST /Tvepisode/{1}/delete
   def delete
     @tvepisode = Tvepisode.find(@params['id'])
@@ -65,7 +41,9 @@ class TvepisodeController < Rho::RhoController
     redirect :action => :index  
   end
   
+  # Updates the episode. Updates the display with the database entries first, then requests the list from the XBMC server
   def update_tvepisode_list
+    WebView.execute_js("showToastLoading('Loading Episodes');")
     @tvepisodes = find_episodes(@@tvseasonid, @@tvshowid)
     unless @tvepisodes.blank?
       WebView.execute_js("updateEpisodeList(#{JSON.generate(@tvepisodes)});")
@@ -73,6 +51,8 @@ class TvepisodeController < Rho::RhoController
     load_tvepisodes(url_for(:action => :update_callback, :query => {:tvshowid => @@tvshowid, :tvseasonid => @@tvseasonid}), @@tvshowid, @@tvseasonid)
   end
   
+  # Update tv episode list callback, will sync the episodes and update the display if the list has changed. 
+  # If there is a connection error will inform the user.
   def update_callback
     if @params['status'] != 'ok'
       error_handle(@params)
@@ -84,9 +64,11 @@ class TvepisodeController < Rho::RhoController
           WebView.execute_js("updateEpisodeList(#{JSON.generate(@tvepisodes)});")
         end
       end
+      WebView.execute_js("hideLoadingToast();")
     end
   end
 
+  # Callback for updating the episodes without updating the display.
   def episodes_callback
     if @params['status'] != 'ok'
       error_handle(@params)
@@ -95,16 +77,27 @@ class TvepisodeController < Rho::RhoController
     end
   end
   
+  # Sends a request to the server to play an episode.
   def play_episode
     unless @params['episodeid'].blank?
-      send_command { Api::V4::Playlist.play_episode(@params['episodeid'], url_for(:action => :play_episode_callback)) }
+      send_command { play_episode_helper(@params['episodeid'], url_for(:action => :play_episode_callback)) }
     end
   end
   
+  # Callback for playing an episode, will inform the user of a connection error.
   def play_episode_callback
-    puts "PLAY/PAUSED"
+    if @params['status'] != 'ok'
+      error_handle(@params)
+      WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
+    else
+      if @params['body']['result']
+        WebView.execute_js("showToastSuccess('Playing Episode');")
+        update_playlist
+      end
+    end
   end
 
+  # Callback for getting Thumbnail. Updates the given episode by the XBMC Library ID, will update the display.
   def episode_thumb_callback
     if @params['status'] != 'ok'
       error_handle(@params)
@@ -120,6 +113,7 @@ class TvepisodeController < Rho::RhoController
     end
   end
 
+  # Requests the thumbnail to be downloaded.
   def get_episode_thumb
     found_episode = find_episode(@params['episodeid'])
     unless found_episode.blank?
@@ -131,12 +125,15 @@ class TvepisodeController < Rho::RhoController
     end
   end
 
+  # Requests the XBMC Server to add the given episode to the playlist queue.
   def add_episode_to_queue
     if @params['episodeid']
-      send_command { Api::V4::Playlist.add_episode(@params['episodeid'], url_for(:action => :episode_queue_callback)) }
+      send_command { queue_episode_helper(@params['episodeid'], url_for(:action => :episode_queue_callback)) }
     end
   end
 
+  # Callback for adding the episode to the playlist. Informations the user of a network error or an error 
+  # trying to add the episode to the queue. Will inform the user if it succeed as well.
   def episode_queue_callback
     puts "BODY === #{@params['body']}"
     if @params['status'] != 'ok'
@@ -144,29 +141,36 @@ class TvepisodeController < Rho::RhoController
       WebView.execute_js("showToastError('#{XbmcConnect.error[:msg]}');")
     else
       if @params['body']['result'] == 'OK'
-        WebView.execute_js("showToastSuccess('Added to the Playlist');");
+        WebView.execute_js("showToastSuccess('Added to the Playlist');")
       else
         WebView.execute_js("showToastError('Can\\\'t add to queue');")
       end
     end
   end
 
+  # Queues all the episodes.
   def play_all_episodes
-    Api::V4::Playlist.clear_video
-    queue_all_episodes
-    Api::V4::Playlist.play_video
+    add_all_to_playlist
   end
 
+  # Adds all the episodes to the playlist. Will add the episodes to the current playlist
+  # Uses synchronous connection to add the episodes. Will inform the user of any error
+  # or will let the user know it succeed.
   def queue_all_episodes
     error = false
-    episodes = find_episodes(@@tvseasonid, @@tvshowid)
-    episodes.each do | episode | 
-      res = Api::V4::Playlist.add_episode(episode.xlib_id)
-      puts "QUEUE ALL EPISODES === #{res['body']}"
-      if (res['status'] != 'ok') || (res['body']['result'] != 'OK')
-        WebView.execute_js("showToastError('Can\\\'t add to queue');")
-        error = true
-        break
+    version = XbmcConfigHelper.current_config.version.to_i
+    unless version.nil?
+      if version == Api::V4::VERSION || (version == 3)
+        episodes = find_episodes(@@tvseasonid, @@tvshowid)
+        episodes.each do | episode | 
+          res = Api::V4::Playlist.add_episode(episode.xlib_id)
+          puts "QUEUE ALL EPISODES === #{res['body']}"
+          if (res['status'] != 'ok') || (res['body']['result'] != 'OK')
+            WebView.execute_js("showToastError('Can\\\'t add to queue');")
+            error = true
+            break
+          end
+        end
       end
     end
     unless error
