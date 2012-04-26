251b772a2ce14cbcc822c1a7352838e2ef57a77f
Richard Race <race.richard@gmail.com>
2012-04-26 21:54:15 +0100

    Added comments fixed methods

--- app/helpers/tv_season_helper.rb
+++ app/helpers/tv_season_helper.rb
@@ -1,22 +1,33 @@
+# Author::    Richard Race (rcr8)
+# Copyright:: Copyright (c) 2012
+# License::   MIT Licence
+
 require 'helpers/movie_helper'
 require 'helpers/method_helper'
 require 'helpers/tv_episode_helper'
 require 'helpers/download_helper'
 
+# Helper for everything to do with TV Seasons.
 module TvSeasonHelper
   include XbmcConfigHelper
   include MethodHelper
   include TvEpisodeHelper
   include DownloadHelper
   
+  # Finds a single Season based upon a XBMC Library ID for Season and TV Show. 
+  # It is filtered by the current active XBMC Configuration
   def find_season(seasonid, tvshowid)
     Tvseason.find(:first, :conditions => {:xbmc_id => XbmcConfigHelper.current_config.object, :tvshow_id => tvshowid, :xlib_id => seasonid})
   end
   
+  # Retrieves an array of Seasons for the given TV Show ID. Filtered by the current active XBMC configuration.
   def find_seasons(tvshowid)
     Tvseason.find(:all, :conditions => {:xbmc_id => XbmcConfigHelper.current_config.object, :tvshow_id => tvshowid})
   end
   
+  # General method to sync tv seasons. The parameter is the list in the XBMC JSON RPC format.
+  # Return either True or False if the list has been updated.
+  # If there no seasons, delete all the episodes associated 
   def sync_seasons(xbmc_seasons, tvshowid)
     unless xbmc_seasons.blank?
       res = handle_new_seasons(xbmc_seasons) || handle_removed_seasons(xbmc_seasons, tvshowid)
@@ -37,6 +48,7 @@ module TvSeasonHelper
     return res
   end
   
+  # Adds new TV Seasons. found in the XBMC library that aren't in the Remote's library.
   def handle_new_seasons(seasons)
     list_changed = false
     seasons.each do | new_season |
@@ -61,6 +73,8 @@ module TvSeasonHelper
     return list_changed
   end
 
+  # Removes any TV Seasons that are in the Remote's library but not in the XBMC's Library
+  # Deletes the episodes if a Season has been removed.
   def handle_removed_seasons(xbmc_tvseasons, tvshowid)
     list_changed = false 
     
@@ -73,6 +87,12 @@ module TvSeasonHelper
         end
       end
       unless got
+        episodes = find_episodes(db_season.xlib_id, tvshowid)
+        episodes.each do | episode |
+          episode.destroy_image
+          episode.destroy
+        end
+
         db_season.destroy_image
         db_season.destroy
         list_changed = true
@@ -81,18 +101,38 @@ module TvSeasonHelper
     return list_changed
   end
 
+  # Selects the JSON API from the Current XBMC Configuration
+  # Only version 3/4 of JSON API is supported.
+  # Updates all the episodes for the TV Show
   def update_episodes(tvshowid)
-    seasons = find_seasons(tvshowid)
-    unless seasons.blank?
-      seasons.each do | season |
-        url_cb = url_for(
-          :controller => :Tvepisode, 
-          :action => :episodes_callback, 
-          :query => {
-            :tvshowid => season.tvshow_id,
-            :tvseasonid => season.xlib_id
-          })
-        send_command { Api::V4::VideoLibrary.get_episodes(url_cb, season.tvshow_id, season.xlib_id) }
+    version = XbmcConfigHelper.current_config.version.to_i
+    unless version.nil?
+      if (version == Api::V4::VERSION)  || (version == 3)
+        seasons = find_seasons(tvshowid)
+        unless seasons.blank?
+          seasons.each do | season |
+            url_cb = url_for(
+              :controller => :Tvepisode, 
+              :action => :episodes_callback, 
+              :query => {
+                :tvshowid => season.tvshow_id,
+                :tvseasonid => season.xlib_id
+              })
+            send_command { Api::V4::VideoLibrary.get_episodes(url_cb, season.tvshow_id, season.xlib_id) }
+          end
+        end
+      end
+    end
+  end
+
+  # Selects the JSON API from the Current XBMC Configuration
+  # Only version 3/4 of JSON API is supported.
+  # Requests to update the seasons for the given TV Show.
+  def update_seasons_helper(callback, tvshowid)
+    version = XbmcConfigHelper.current_config.version.to_i
+    unless version.nil?
+      if (version == Api::V4::VERSION)  || (version == 3)
+        Api::V4::VideoLibrary.get_seasons(callback, tvshowid)
       end
     end
   end
