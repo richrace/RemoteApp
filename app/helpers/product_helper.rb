edaf3ffab6788be87d39debe00e284fbbf34a028
Richard Race <race.richard@gmail.com>
2012-05-01 12:38:30 +0100

    Removed Google API

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -13,7 +13,7 @@ module ProductHelper
       @barcode = code
       @country = country
       
-      google_api_key = "AIzaSyD6zfGyGIia-TF9cW4wf85xnz4mvM6RSto"
+      google_api_key = ""
       
       Rho::AsyncHttp.get(
         :url => "https://www.googleapis.com/shopping/search/v1/public/products?country=#{@country}&q=#{@barcode}&maxResults=5&key=#{google_api_key}",
